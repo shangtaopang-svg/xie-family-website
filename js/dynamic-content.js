7bc2d478dc75c1f4c1db5d09e7d729cd5af0a926
尚桃邦-SVG <shangtaopang@qq.com>
2026-06-09 13:31:46 +0800

    add: 777.mp4 to video carousel

--- js/dynamic-content.js
+++ js/dynamic-content.js
@@ -82,7 +82,8 @@
         // Always prepend promo videos first
         var allVids = [
           { id: 'promo1', src: '/video/promo1.mp4', title: '宣传片1', poster: '/images/carousel/123.jpg' },
-          { id: 'promo2', src: '/video/promo2.mp4', title: '宣传片2', poster: '/images/carousel/123.jpg' }
+          { id: 'promo2', src: '/video/promo2.mp4', title: '宣传片2', poster: '/images/carousel/123.jpg' },
+          { id: 'v777', src: '/video/777.mp4', title: '视频777', poster: '/images/carousel/123.jpg' }
         ].concat(videos);
 
         if (!allVids.length) {
